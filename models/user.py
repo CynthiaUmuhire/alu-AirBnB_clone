851f98c82feb701ade93546677b9babecf3d8f19
Kaluval <v.kalu@alustudent.com>
2023-05-18 13:25:31 +0200

    user class update

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This Is A Module That Creates A Uaer Class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User Object Management Class"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
