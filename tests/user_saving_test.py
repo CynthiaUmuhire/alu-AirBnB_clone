9fa31174e07ea05725f8788e1e53b1654cebef09
Kaluval <v.kalu@alustudent.com>
2023-05-22 14:59:32 +0200

    uder_saving_test

--- tests/user_saving_test.py
+++ tests/user_saving_test.py
@@ -3,31 +3,40 @@
 
 from models import storage
 from models.user import User
-from models.base_model import BaseModel
 
-all_objs = storage.all()
-print ("__Reloades bjects__")
-for obj_id in all_objs.keys():
-    object = all_objs[obj_id]
-    print(object)
+def print_all_objects():
+    """Prints all the objects in the storage"""
+    all_objs = storage.all()
+    print("__Reloaded objects__")
+    for obj_id, obj in all_objs.items():
+        print(obj)
 
-print(" --Create the first user--")
-user1 = User()
-user1.first_name = "Nate"
-user1.last_name = "DUNKAN"
-user1.email = "Ndunkan@gmail.com"
-user1.password = "L.o.v.e"
+def create_user(first_name, last_name, email, password):
+    """Creates a new User instance"""
+    user = User()
+    user.first_name = first_name
+    user.last_name = last_name
+    user.email = email
+    user.password = password
+    return user
 
-print(" --Create the second user--")
-user2 = User()
-user2.first_name = "Kristina"
-user2.last_name = "Michaelson"
-user2.email = "kristina@gmail.com"
-user2.password = "come"
+def main():
+    """Main function"""
+    print_all_objects()
 
-print(" --Create the third user--")
-user3 = User()
-user3.first_name = "Iyanya"
-user3.last_name = "Rugger"
-user3.email = "dormant@gmail.com"
-user3.password = "HeLLo"
+    print("-- Create the first user --")
+    user1 = create_user("Nate", "DUNKAN", "Ndunkan@gmail.com", "L.o.v.e")
+    storage.save()
+
+    print("-- Create the second user --")
+    user2 = create_user("Kristina", "Michaelson", "kristina@gmail.com", "come")
+    storage.save()
+
+    print("-- Create the third user --")
+    user3 = create_user("Iyanya", "Rugger", "dormant@gmail.com", "HeLLo")
+    storage.save()
+
+    print_all_objects()
+
+if __name__ == '__main__':
+    main()
