b6550bfec69bfc22c36f8d2cbb6dc07bce092a77
Kaluval <v.kalu@alustudent.com>
2023-05-24 20:45:28 +0200

    amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ """
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
+class TestAmenity(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
@@ -17,3 +18,7 @@ class test_Amenity(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+
+if __name__ == '__main__':
+    unittest.main()
